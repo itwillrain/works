a84d80b6adb796cdda531e91216f85254e54570a
Matsushita Takeshi <matsushita@neopa.jp>
2021-03-13 06:09:36 +0900

    :+1:          機能改善 (fix/feat)
    レイアウト修正

--- packages/front/src/pages/about/index.vue
+++ packages/front/src/pages/about/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container class="fill-height">
+  <v-container>
     <v-row>
       <v-col>
         <v-row justify="center">
--- packages/front/src/pages/works/index.vue
+++ packages/front/src/pages/works/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container class="fill-height">
+  <v-container>
     <v-row v-if="pickupProjects">
       <v-col>
         <v-row>
