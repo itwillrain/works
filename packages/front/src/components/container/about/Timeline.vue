c6da8c77ab99f33c43ec88e43e01f15f98a73af0
Matsushita Takeshi <matsushita@neopa.jp>
2021-03-13 06:00:31 +0900

    :+1:          機能改善 (fix/feat) デザイン調整

--- packages/front/src/components/container/about/Timeline.vue
+++ packages/front/src/components/container/about/Timeline.vue
@@ -7,7 +7,9 @@
       small
     >
       <div class="py-4">
-        <h3 :class="`font-weight-light mb-4`">{{ year.year }}</h3>
+        <h3 :class="`font-weight-500 mb-4 ${year.color}--text`">
+          {{ year.year }}
+        </h3>
         <div>{{ year.title }}</div>
       </div>
     </v-timeline-item>
@@ -28,37 +30,37 @@ export default defineComponent({
     const years = ref<Timeline[]>([
       {
         color: 'cyan',
-        year: '2004年3月',
+        year: 'May 2004',
         title: '奈良市立一条高等学校普通科卒業',
       },
       {
         color: 'cyan',
-        year: '2008年3月',
+        year: 'May 2008',
         title: '大阪芸術大学デザイン学科卒業',
       },
       {
         color: 'cyan',
-        year: '2008年6月',
+        year: 'June 2008',
         title: '有限会社ROVEX入社',
       },
       {
         color: 'cyan',
-        year: '2013年6月',
+        year: 'June 2013',
         title: '有限会社ROVEX退社',
       },
       {
         color: 'cyan',
-        year: '2014年6月',
+        year: 'June 2014',
         title: 'イギリス Mayfair School of English（2015年4月まで）',
       },
       {
         color: 'cyan',
-        year: '2015年11月',
+        year: 'Nov 2015',
         title: 'フィリピンSMEAG スパルタ校語学留学（2016年3月まで）',
       },
       {
         color: 'cyan',
-        year: '2017年9月〜',
+        year: 'Sep 2017',
         title: '株式会社NEOPA 入社',
       },
     ])
