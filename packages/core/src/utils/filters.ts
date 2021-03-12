540e0d086aa4e391357c718b3c77aea08e7eeb1d
Matsushita Takeshi <matsushita@neopa.jp>
2021-03-12 19:03:34 +0900

    :+1:          機能改善 (fix/feat)
    filter追加

--- packages/core/src/utils/filters.ts
+++ packages/core/src/utils/filters.ts
@@ -1,5 +1,23 @@
 import { Timestamp } from '../types/common-property';
 import { format as $format } from 'date-fns'
+
+/**
+ * 日付をフォーマット
+ * @param {firebase.firestore.Timestamp} timestamp
+ * @param {string} format
+ * @return {string}
+ */
 export const formatDate = (ts: Timestamp, format="yyyy年MM月dd日") => {
+  if(!ts) return ts;
   return $format(ts.toDate(), format)
+}
+/**
+ * 文字列の頭文字だけを大文字にして返す
+ * @param {string} value
+ * @return {string}
+ */
+ export const initialUpperCase = (value: string): string => {
+  if (!value) return ''
+  if (typeof value !== 'string') return ''
+  return value.charAt(0).toUpperCase()
 }
