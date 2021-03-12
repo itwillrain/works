96d6ba4b6822750c71d477842b3c1957d63ceaab
Matsushita Takeshi <matsushita@neopa.jp>
2021-03-13 04:25:44 +0900

    :bug:         バグ修正 (fix) デザイン崩れ修正

--- packages/front/src/pages/about/index.vue
+++ packages/front/src/pages/about/index.vue
@@ -1,36 +1,38 @@
 <template>
   <v-container class="fill-height">
-    <v-row justify="center">
-      <v-col cols="12" md="8">
-        <h2 class="text-center">About</h2>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col>
-        <v-row>
-          <!-- Personal info -->
-          <v-col cols="12" md="4">
-            <c-about-aside />
-          </v-col>
-          <!-- Resume -->
-          <v-col v-if="resume" cols="12" md="8">
-            <v-tabs v-model="tab" background-color="transparent" class="mb-4">
-              <v-tab>Profile</v-tab>
-              <v-tab>Life log</v-tab>
-            </v-tabs>
+    <div>
+      <v-row justify="center">
+        <v-col cols="12" md="8">
+          <h2 class="text-center">About</h2>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-row>
+            <!-- Personal info -->
+            <v-col cols="12" md="4">
+              <c-about-aside />
+            </v-col>
+            <!-- Resume -->
+            <v-col v-if="resume" cols="12" md="8">
+              <v-tabs v-model="tab" background-color="transparent" class="mb-4">
+                <v-tab>Profile</v-tab>
+                <v-tab>Life log</v-tab>
+              </v-tabs>
 
-            <v-tabs-items v-model="tab" :class="$style.tab__items">
-              <v-tab-item>
-                <nuxt-content :document="resume" />
-              </v-tab-item>
-              <v-tab-item>
-                <c-about-timeline />
-              </v-tab-item>
-            </v-tabs-items>
-          </v-col>
-        </v-row>
-      </v-col>
-    </v-row>
+              <v-tabs-items v-model="tab" :class="$style.tab__items">
+                <v-tab-item>
+                  <nuxt-content :document="resume" />
+                </v-tab-item>
+                <v-tab-item>
+                  <c-about-timeline />
+                </v-tab-item>
+              </v-tabs-items>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </div>
   </v-container>
 </template>
 <script lang="ts">
