8374cc52a264bd6b79a51331ae7244a2a48ec385
Matsushita Takeshi <matsushita@neopa.jp>
2021-03-14 05:05:50 +0900

    :bug:         バグ修正 (fix) OGP path修正

--- packages/front/nuxt.config.ts
+++ packages/front/nuxt.config.ts
@@ -44,7 +44,7 @@ const config: NuxtConfig = {
       {
         hid: 'og:image',
         property: 'og:image',
-        content: `${envSet.BASE_URL}/OGP.jpg`,
+        content: `${envSet.BASE_URL}/ogp-img.jpg`,
       },
       { hid: 'description', name: 'description', content: desc },
       {
