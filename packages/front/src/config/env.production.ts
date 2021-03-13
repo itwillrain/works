605fbc061e438d6ecc0acc5b69fc9632edbecca7
Matsushita Takeshi <matsushita@neopa.jp>
2021-03-14 03:12:20 +0900

    :green_heart: テストやCIの修正・改善 (test) 本番用BUILD追加

--- packages/front/src/config/env.production.ts
+++ packages/front/src/config/env.production.ts
@@ -1,1 +1,10 @@
-module.exports = {}
+module.exports = {
+  BASE_URL: 'itwillrain.com',
+  FIREBASE_API_KEY: 'AIzaSyARranLMi6hHwRa2HYjyUXW0I3cpnTF_Tw',
+  FIREBASE_AUTH_DOMAIN: 'itwillrain-portfolio.firebaseapp.com',
+  FIREBASE_DATABASE_URL: 'https://itwillrain-portfolio.firebaseio.com',
+  FIREBASE_PROJECT_ID: 'itwillrain-portfolio',
+  FIREBASE_STORAGE_BUCKET: 'itwillrain-portfolio.appspot.com',
+  FIREBASE_MESSAGING_SENDER_ID: '651397035800',
+  FIREBASE_APP_ID: '1:651397035800:web:e2049dd978f52df241106d',
+}
