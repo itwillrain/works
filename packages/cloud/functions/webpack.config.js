b8e3dd0a2aba8ed25a9725a49e1859710d8ba9d8
Matsushita Takeshi <matsushita@neopa.jp>
2021-03-19 01:31:53 +0900

    :shirt:       Lintエラーの修正やコードスタイルの修正 (style)　functionsにprettierの追加

--- packages/cloud/functions/webpack.config.js
+++ packages/cloud/functions/webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
+        test: /\.ts?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
       },
@@ -21,7 +21,7 @@ module.exports = {
   },
   devtool: 'nosources-source-map',
   resolve: {
-    extensions: ['.tsx', '.ts', '.js', '.json'],
+    extensions: ['.ts', '.js', '.json'],
   },
   externals: [
     /^firebase.+$/,
